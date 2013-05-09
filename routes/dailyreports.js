f0a3219c02547fcbfc7b98ab91c3c32d31ef0e95
hudongyang <whudongyang@gmail.com>
2013-05-09 09:09:24 +0800

    dailutreports

---
+++ routes/dailyreports.js
@@ -0,0 +1,27 @@
+module.exports = {
+	index: function(req, res) {
+		var find = DailyReport.findAll();
+		
+		find.once('success', function(items) {
+			res.render('dailyreports/index.ejs', {title: '日报列表', dailyreports: items});
+		});
+	},
+	n: function(req, res){
+		res.render('dailyreports/new.ejs', {title: '添加日报'});
+	},
+	create:function(req, res){
+		
+	},
+	edit:function(req, res){
+		
+	},
+	update:function(req, res){
+		
+	},
+	show:function(req, res){
+		
+	},
+	check: function(req,res){
+		res.end("0");
+	}
+};
